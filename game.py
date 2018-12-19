c3e31c3d9e43b3ce9d33c163d772a60364a623f9
Jocapear <jose18_carl@hotmail.com>
2018-12-18 19:38:55 -0600

    Visualization with graphics now working

--- game.py
+++ game.py
@@ -1,29 +1,28 @@
 from graphics import *
+from world import World
+
+def showFrame(win, winSize, space, spaceSize):
+    pixelSize = winSize//spaceSize
+    for i in range(spaceSize):
+        for j in range(spaceSize):
+            pixel = Rectangle(Point(j*pixelSize,i*pixelSize), Point((j*pixelSize)+pixelSize, (i*pixelSize)+pixelSize))
+            if space[i][j]:
+                pixel.setFill("white")
+            else:
+                pixel.setFill("black")
+            pixel.draw(win)
+    update()
 
 def main():
-    win = GraphWin('Game', 1000, 700) # give title and dimensions
-
-    head = Circle(Point(40,100), 25) # set center and radius
-    head.setFill("yellow")
-    head.draw(win)
-
-    eye1 = Circle(Point(30, 105), 5)
-    eye1.setFill('blue')
-    eye1.draw(win)
-
-    eye2 = Line(Point(45, 105), Point(55, 105)) # set endpoints
-    eye2.setWidth(3)
-    eye2.draw(win)
-
-    mouth = Oval(Point(30, 90), Point(50, 85)) # set corners of bounding box
-    mouth.setFill("red")
-    mouth.draw(win)
-
-    label = Text(Point(100, 120), 'A face')
-    label.draw(win)
-
-    message = Text(Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
-    message.draw(win)
+    world = World()
+    gameSize = 100
+    windowSize = 700
+    world.initialize(gameSize)
+    win = GraphWin('Game', windowSize, windowSize, autoflush=False) # give title and dimensions
+    while True:
+        #time.sleep(.2)
+        world.nextFrame()
+        showFrame(win, windowSize, world.space, world.size)
     win.getMouse()
     win.close()
 
